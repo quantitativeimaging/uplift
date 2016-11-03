f289579e2e12d15670bc5e801d0ff6cd21abcc7b
AD\ejr36 <ejr36@cam.ac.uk>
2016-11-03 16:48:52 +0000

    Added video particle tracking and a basic method to infer velocity field from video-tracked data

--- trackuplift.m
+++ trackuplift.m
@@ -35,11 +35,15 @@ lpIm = lpIm + 1;
 v.CurrentTime = 20 + lpIm*2;
 
 imDat = readFrame(v); % skip a few frames ahead
+% figure(1)
+% imshow(imDat)
 
 % Select a region containing only particles
 % Try finding mean grey level (could use red channel)
 imROI = mean( imDat(330:(330+689), 370:(370+759), :), 3); 
+% figure(2)
 % imagesc(imROI)
+% colormap(gray)
 
 if(flagBGgs)
   imBG = imgaussfilt(imROI, radgauss);
@@ -50,6 +54,7 @@ end
     
 figure(4)
 imagesc(im1)
+colormap(gray)
 
 im2 = bpass(im1, 1, 5);
 figure(5)
@@ -94,3 +99,80 @@ hold on
  legend('track','start','end')
 hold off
 
+
+% --------------------------------------------
+% % Now find initial and displaced positions,
+% Try arbitrary frames (1 and 10) at first:
+numTracks = (res(end,4));
+posInit = -ones(numTracks,2);
+posDisp = -ones(numTracks,2);
+for lpTrack = 2:numTracks
+   
+    myCoords = res((res(:,4)==lpTrack),[1:4]);
+    if( ((sum(myCoords(:,3)==1))+(sum(myCoords(:,3)==10)))==2 )
+    posInit(lpTrack,:) = myCoords((myCoords(:,3)==1),1:2);
+    posDisp(lpTrack,:) = myCoords((myCoords(:,3)==10),1:2);
+    end
+end
+
+invalid = (posInit(:,1)==-1) | (posDisp(:,1)==-1);
+
+posInit(invalid,:) = [];
+posDisp(invalid,:) = [];
+
+figure(10)
+imagesc(imROI)
+colormap(gray)
+hold on
+  scatter(posInit(:,1), posInit(:,2), 'c');
+  scatter(posDisp(:,1), posDisp(:,2), 'r');
+hold off
+legend('frame 1', 'frame 10')
+axis equal
+
+% ---------------------------- CP 2 TFORM
+mytform = cp2tform(posDisp, posInit, 'lwm');
+% 'piecewise linear' is stricter than local weighted mean 'lwm'
+
+[XX,YY] = meshgrid([100:25:600], [100:25:600]);
+
+listXinit = XX(:);
+listYinit = YY(:);
+
+[listXfinal, listYfinal] = tforminv(mytform, listXinit, listYinit);
+
+listDisps = sqrt((listXfinal - listXinit).^2 + (listYfinal - listYinit).^2);
+matrDisps = reshape(listDisps, size(XX));
+
+figure(11)
+scatter(listXinit, -listYinit, 'r')
+hold on
+  scatter(listXfinal, -listYfinal, 'c')
+hold off
+
+figure(12)
+imagesc(imROI)
+hold on
+  scatter(listXinit, listYinit, 'r')
+  scatter(listXfinal, listYfinal, 'c')
+hold off
+axis equal
+
+figure(13)
+imagesc(imROI)
+colormap(gray)
+hold on
+for lpPt = 1:size(listXinit,1)
+   plot([listXinit(lpPt);listXfinal(lpPt)],...
+        [listYinit(lpPt);listYfinal(lpPt)], 'r', 'lineWidth', 1) 
+end
+hold off
+axis equal
+
+figure(14)
+mesh(XX, YY, matrDisps)
+colormap(jet)
+xlabel('X position, pixels', 'fontSize', 14)
+ylabel('Y position, pixels', 'fontSize', 14)
+zlabel('speed, pixel per 9 frames', 'fontSize', 14)
+set(gca, 'fontSize', 12)
